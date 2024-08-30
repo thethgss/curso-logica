7a3abc968857a7cfbdb7b340a15bcbbb46388dc4
thethgss <thiagoclemente221@gmail.com>
2024-08-30 20:53:21 -0300

    Create exec-2.py

---
+++ exec-2.py
@@ -0,0 +1,4 @@
+#2. Etiqueta - Elabore um programa que escreve seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+print("Thiago")
+print("Rio de Janeiro")
+print("Rio de Janeiro")
