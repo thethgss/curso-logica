e5b766dd024c458d5cb70c96c0a32f28a24c6db0
thethgss <thiagoclemente221@gmail.com>
2024-08-30 21:00:45 -0300

    Create exec-7.py

---
+++ exec-7.py
@@ -0,0 +1,12 @@
+#7. Pinheiro  - Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.
+print("       x")
+print("      XXX")
+print("     XXXXX")
+print("    XXXXXXX")
+print("   XXXXXXXXX")
+print("  XXXXXXXXXXX")
+print(" XXXXXXXXXXXXX")
+print("XXXXXXXXXXXXXXX")
+print("       XX")
+print("       XX")
+print("      XXXX")
